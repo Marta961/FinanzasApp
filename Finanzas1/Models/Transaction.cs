50bbfadc1f04eac88aa5a983c5c2aa34b323c5eb
Marta961 <marta.grau@alu.ceacfp.es>
2026-02-19 16:58:58 +0100

    Update Transaction.cs

--- Finanzas1/Models/Transaction.cs
+++ Finanzas1/Models/Transaction.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }                 // PK
         public DateTime Date { get; set; } = DateTime.Today;
         public string Description { get; set; } = "";
-        public decimal Amount { get; set; }         // +income, -expense (or add Type)
+        public decimal Amount { get; set; }         // +ingresos, -gastos
         public string Category { get; set; } = "";
     }
 }
+
